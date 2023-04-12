c3ee19de3a7ee4a4b75e1e02a2db4cb83ec08289
takinaaakawaii <123395074+takinaaakawaii@users.noreply.github.com>
2023-04-12 20:20:09 +0700

    Create chat.cpp

---
+++ chat.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string name = "Bcrepi";
+    cout << "I don't need" << name << " server chat" << endl;
+    cout << "I want cheat fuck you" << name;
+    return 0;
+}
